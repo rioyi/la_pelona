0c2f04c92cdcce0f5a6701889d8b74217f0935f3
Wilbert <wilbert.angarita@eirplus.com>
2016-11-28 14:02:21 -0400

    Se corrio las migraciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128172321) do
+ActiveRecord::Schema.define(version: 20161128175602) do
+
+  create_table "ages", force: :cascade do |t|
+    t.string   "edad"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sexes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sickness_generals", force: :cascade do |t|
     t.string   "name"
